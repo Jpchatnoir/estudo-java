e6aefd368a6b467dcf2d1305cfeec9571ac2ae44
Joao Pedro <canadajoaoalmeida@gmail.com>
2024-04-26 02:19:09 -0300

    update

--- projeto Java/tipos-variaveis/src/TiposVariaveis.java
+++ projeto Java/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    public static void main(String[] args) {
+		byte idade = 123;
+		short ano = 2021;
+		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+		float pi = 3.14F;
+		double salario = 1275.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+		numero = 10;
+        System.out.print(numero);
+	}
 }
+
